04038cceec0a7fd26c83b3ce33cd6e02e7079f7f
Lich_Amnesia <alwaysxiaop@gmail.com>
2016-08-31 00:04:26 -0600

    add 3

--- 3.Longest_Substring.cpp
+++ 3.Longest_Substring.cpp
@@ -2,7 +2,7 @@
 * @Author: Lich_Amnesia
 * @Date:   2016-08-30 23:05:19
 * @Last Modified by:   Lich_Amnesia
-* @Last Modified time: 2016-08-30 23:49:07
+* @Last Modified time: 2016-08-31 00:03:56
 * @3. Longest Substring Without Repeating Characters
 */
 
@@ -15,25 +15,29 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int next[1000];
+        if (s.length() == 1){
+            return 1;
+        } 
         int maxLen = 0;
-        int j = 0;
+        int j = -1;
         for (int i = 0; i < 1000;i ++){
             next[i] = -1;
         }
         for (int i = 0; i < s.length(); i ++){
-            j = max(next[s[i]-'a'], j);
+            j = max(next[s[i]], j);
             maxLen = max(maxLen, i - j);
             // for (int k = 0; k < 10; k ++){
             //     printf("%d ", next[k]);
             // }cout << endl;
-            next[s[i] - 'a'] = i;
+            // printf("%d\n", j);
+            next[s[i]] = i;
         }
         return maxLen;
     }
 };
 
 int main(){
-    string s = "vvv";
+    string s = "abcabcd";
     Solution ans;
     cout << ans.lengthOfLongestSubstring(s);
     return 0;
