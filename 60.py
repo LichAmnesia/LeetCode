510b3d82cd720ef8257a71b2bbe8e70399e5d40d
Lich_Amnesia <alwaysxiaop@gmail.com>
2016-10-14 18:30:23 -0600

    Update Problem 60

--- 60.py
+++ 60.py
@@ -2,5 +2,24 @@
 # @Author: Lich_Amnesia
 # @Email: alwaysxiaop@gmail.com
 # @Date:   2016-10-14 16:26:03
-# @Last Modified time: 2016-10-14 16:26:03
+# @Last Modified time: 2016-10-14 16:29:06
 # @FileName: 60.py
+
+
+class Solution(object):
+    def getPermutation(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: str
+        """
+        r = ''; fac = [1]*(n+1); s = []
+        for i in xrange(1, n+1):
+            fac[i] = i*fac[i-1]
+            s.append(i)
+        for i in xrange(n, 0, -1):
+            ind = (k-1)/fac[i-1]
+            r += str(s[ind])
+            del s[ind]
+            k %= fac[i-1]
+        return r
