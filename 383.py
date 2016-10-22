3e292467e6c76da69db03cbbc881dcf308e6c115
Lich_Amnesia <alwaysxiaop@gmail.com>
2016-10-21 22:52:03 -0600

    Add Problem 383

--- 383.py
+++ 383.py
@@ -1,16 +1,6 @@
 # -*- coding: utf-8 -*-
-# @Author: Lich_Amnesia
+# @Author: Lich Amnesia
 # @Email: alwaysxiaop@gmail.com
-# @Date:   2016-09-18 15:57:30
+# @Date:   2016-09-18 16:06:47
+# @Last Modified by:   Lich Amnesia
 # @Last Modified time: 2016-09-18 15:58:08
-# @FileName: 383.py
-
-
-class Solution(object):
-    def canConstruct(self, ransomNote, magazine):
-        """
-        :type ransomNote: str
-        :type magazine: str
-        :rtype: bool
-        """
-        return not collections.Counter(ransomNote) - collections.Counter(magazine)
