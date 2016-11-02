12a0c9b360f4fe72116a087bcfb981f2bb92171b
Lich_Amnesia <alwaysxiaop@gmail.com>
2016-11-01 22:03:04 -0600

    add 3 problems

--- 283.py
+++ 283.py
@@ -2,7 +2,7 @@
 # @Author: Lich_Amnesia
 # @Email: alwaysxiaop@gmail.com
 # @Date:   2016-09-18 15:30:02
-# @Last Modified time: 2016-09-18 15:35:43
+# @Last Modified time: 2016-10-30 20:48:27
 # @FileName: 283.py
 
 
@@ -19,3 +19,20 @@ class Solution(object):
                 num_of_zeros += 1
         for i in range(num_of_zeros):
             nums[len(nums) - num_of_zeros + i] = 0
+
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[j] = nums[i]
+                j += 1
+        
+        while j < len(nums):
+            nums[j] = 0
+            j += 1
