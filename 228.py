66ada9e3ec7df23455c9fe393d21ef9bc903ca2a
LichAmnesia <alwaysxiaop@gmail.com>
2016-12-02 23:34:52 -0700

    ADD 2 PROBLEMS

--- 228.py
+++ 228.py
@@ -2,7 +2,7 @@
 # @Author: Lich_Amnesia
 # @Email: alwaysxiaop@gmail.com
 # @Date:   2016-10-07 15:04:32
-# @Last Modified time: 2016-10-07 15:13:44
+# @Last Modified time: 2016-11-29 12:56:33
 # @FileName: 228.py
 
 
@@ -20,3 +20,33 @@ class Solution(object):
                 ans += ranges
             ranges[1:] = nums[i]
         return ['->'.join(map(str, r)) for r in ans]
+
+
+class Solution(object):
+    def summaryRanges(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[str]
+        """
+        ans, ranges = [], []
+        for i in range(len(nums)):
+            if nums[i] - 1 not in ranges:
+                ans.append(ranges[:])
+                print(ans)
+                ranges = [nums[i]]
+            else:
+                if len(ranges) < 2:
+                    ranges.append(nums[i])
+                else:
+                    ranges[1] = nums[i]
+                print(ranges)
+        ans.append(ranges[:])
+        print(ans)
+        res = []
+        for x in ans:
+            if x:
+                if type(x) == int:
+                    res.append(str(x))
+                else:
+                    res.append('->'.join(map(str,x)))
+        return res
--- 345.py
+++ 345.py
@@ -2,29 +2,31 @@
 # @Author: Lich_Amnesia
 # @Email: alwaysxiaop@gmail.com
 # @Date:   2016-09-20 00:10:36
-# @Last Modified time: 2016-09-20 00:42:31
+# @Last Modified time: 2016-11-29 13:07:02
 # @FileName: 345.py
 
 
 class Solution(object):
-
     def reverseVowels(self, s):
         """
         :type s: str
         :rtype: str
         """
-        res = []
+        vowels = []
         for i in range(len(s)):
-            if s[i] in ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']:
-                res.append(i)
-        revese = res[::-1]
-        t = ''
-        now = 0
-        for i in range(len(res)):
-            t += s[now:res[i]] + s[revese[i]]
-            now = res[i] + 1
-        t += s[now:]
-        return t
+            if s[i] in 'aeiouAEIOU':
+                vowels.append(i)
+        # print(vowels_re)
+        vowels_re = vowels[::-1]
+        # print(vowels_re)
+        news = []
+        pos = 0
+        for i in range(len(vowels)):
+            # print(pos, vowels[i], vowels_re[i])
+            news.append(s[pos:vowels[i]] + s[vowels_re[i]])
+            pos = vowels[i] + 1
+        news.append(s[pos:])
+        return ''.join(news)
 
     # def reverseVowels(self, s):
     #     vowels = re.findall('(?i)[aeiou]', s)
